e6d8f7863b1ef73d478b3a48776507dfda90d7cd
jasontbaker99 <70231887+jasontbaker99@users.noreply.github.com>
2020-09-06 12:37:48 -0700

    Update question3.js

--- practicum/week-1/question3.js
+++ practicum/week-1/question3.js
@@ -9,5 +9,13 @@ There are 5 ordered pairs: (4, 6), (4, 9), (2, 6), (2, 9), (6, 9)
 */
 
 var numberOfOrderedPairs = function(input) {
-
+  let mx = 0;
+  for(var i=0;i<input.length; i++){
+    for(var j=i;j<input.length; j++){
+      if(input[i] < input[j]){
+        mx++;
+      }
+    }
+  }
+  return mx;
 };
