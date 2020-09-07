b44deba2252d245568acda3c550c6cccad17199d
jasontbaker99 <70231887+jasontbaker99@users.noreply.github.com>
2020-09-06 23:54:25 -0700

    Update question4.js

--- practicum/week-2/question4.js
+++ practicum/week-2/question4.js
@@ -13,5 +13,12 @@ Example:
 */
 
 var hasDuplicates = function(input) {
-
+  for(var i=0;i<input.length;i++){
+    for(var j=0;j<input.length;j++){
+      if(i != j && input[i] === input[j]){
+        return true;
+      }
+    }
+  }
+  return false;
 };
