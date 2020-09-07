4e61c8ecd365218d27fed13188e598d434b9004e
jasontbaker99 <70231887+jasontbaker99@users.noreply.github.com>
2020-09-06 23:55:42 -0700

    Update question3.js

--- practicum/week-2/question3.js
+++ practicum/week-2/question3.js
@@ -15,10 +15,10 @@ output: [
 var PascalTriangle = function(n) {
   let row = [];
   let triangle = [1];
-  for(var i=0;i<n;i++){
+  for(var i=0;i<n-1;i++){
     let temp = row
     row = [1];
-    for(var j=0;j<temp.length-2;j++){
+    for(var j=0;j<temp.length-1;j++){
       row.push((temp[j]+temp[j+1]));
     }
     row.push(1);
