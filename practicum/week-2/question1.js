10c11cce0ae9619051fcdc6450acf80a1b7ff9eb
jasontbaker99 <70231887+jasontbaker99@users.noreply.github.com>
2020-09-06 23:32:53 -0700

    Update question1.js

--- practicum/week-2/question1.js
+++ practicum/week-2/question1.js
@@ -10,5 +10,18 @@ is at least twice as much as every other number
 */
 
 var largestNumberIsAtLeastTwice = function(input) {
-   
+   let mx = 0;
+   let index = 0;
+   for(var i=0;i<input.length;i++){
+     if(input[i] > mx){
+       mx = input[i];
+       index = i;
+     }
+   }
+   for(i=0;i<input.length;i++){
+     if(input[i]*2 >= mx && index != i){
+       return false;
+     }
+   }
+   return true;
 };
