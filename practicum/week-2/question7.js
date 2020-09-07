0e261ceb0bb7f7260c7c4ef96b94aef3d8f480e0
jasontbaker99 <70231887+jasontbaker99@users.noreply.github.com>
2020-09-07 14:01:09 -0700

    Update question7.js

--- practicum/week-2/question7.js
+++ practicum/week-2/question7.js
@@ -11,6 +11,15 @@ output: [[1,4], [2,5], [3,6]]
 
 */
 
-var transpose = function(M) {
-
+var transpose = function(m) {
+  let n = [];
+  for(var i=0;i<m[0].length;i++){
+    let r = [];
+    for(var j=0;j<m.length;j++){
+      let temp = m[j];
+      r.push(temp[i]);
+    }
+    n.push(r);
+  }
+  return n;
 };
