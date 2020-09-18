39f7b1433e0dc99b20f01e82f5f7c4aa30e223f7
jasontbaker99 <70231887+jasontbaker99@users.noreply.github.com>
2020-09-18 00:10:04 -0700

    Update question10.js

--- practicum/week-3/question10.js
+++ practicum/week-3/question10.js
@@ -12,13 +12,18 @@ output: true
 
 var isPalindrome = function(s) {
   while(s.includes(" ")){
-    s = s.replace(" ","");
+    s = s.replace(" ", '');
+    s = s.replace("?", '');
+    s = s.replace("!", '');
+    s = s.replace(",", '');
+    s = s.replace(".", '');
   }
+  s = s.toLowerCase();
   let len = 0;
   if(s.length%2 == 0){
     len = s.length/2;
   }else{
-    len =  (s.length-1)/2
+    len = (s.length-1)/2
   }
   for(let i=0;i<len;i++){
     if(s[i] !== s[s.length-1-i]){
